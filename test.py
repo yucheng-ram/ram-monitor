039895a50cc6a7453262e305719a0cf36a0549fc
yucheng-ram <lovechaiu99@gmail.com>
2026-03-03 21:50:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Hello RAM Monitor")
+import requests
+import os
+
+webhook_url = os.environ["DISCORD_WEBHOOK"]
+
+message = {
+    "content": "✅ RAM Monitor 雲端測試成功！"
+}
+
+response = requests.post(webhook_url, json=message)
+
+print("Discord notification sent.")
